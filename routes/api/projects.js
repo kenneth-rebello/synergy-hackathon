01c3aeb74248b1d245f7f1505fbb8494154963de
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2019-08-31 14:45:37 +0530

    New Commit

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -27,91 +27,93 @@ router.get('/', async function(req, res){
     }
 });
 
-// // ML
-// router.get('/ml', async function(req, res){
-//     try{
+// ML
+router.get('/ml', async function(req, res){
+    try{
 
-//         const uploads = await Project.find({'domain': "ML"}).populate('uploader',['rollNo', 'dept','year']);
-//         console.log(uploads);
-//         if(uploads == []){     
-//             res.render('projects.ejs',{msg:'No Projects To Show'});
-//         }else{
-//             res.render('projects.ejs',{uploads});
-//         }
+        const uploads = await Project.find({'domain': "ML"}).populate('uploader',['rollNo', 'dept','year']);
+        console.log(uploads);
+        if(uploads == []){     
+            res.render('projects.ejs',{msg:'No Projects To Show'});
+        }else{
+            res.render('projects.ejs',{uploads});
+        }
         
 
-//     }catch(err){
+    }catch(err){
         
-//         console.error(err.message);
-//         res.status(500).send('Server Error');
-//     }
-// });
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
-// // WB
-// router.get('/ml', async function(req, res){
-//     try{
+// WB
+router.get('/ml', async function(req, res){
+    try{
 
-//         const uploads = await Project.find({'domain': "WB"}).populate('uploader',['rollNo', 'dept','year']);
-//         console.log(uploads);
-//         if(uploads == []){     
-//             res.render('projects.ejs',{msg:'No Projects To Show'});
-//         }else{
-//             res.render('projects.ejs',{uploads});
-//         }
-        
-
-//     }catch(err){
-        
-//         console.error(err.message);
-//         res.status(500).send('Server Error');
-//     }
-// });
-
-// // DSA
-// router.get('/ml', async function(req, res){
-//     try{
-
-//         const uploads = await Project.find({'domain': "DSA"}).populate('uploader',['rollNo', 'dept','year']);
-//         console.log(uploads);
-//         if(uploads == []){     
-//             res.render('projects.ejs',{msg:'No Projects To Show'});
-//         }else{
-//             res.render('projects.ejs',{uploads});
-//         }
+        const uploads = await Project.find({'domain': "WB"}).populate('uploader',['rollNo', 'dept','year']);
+        console.log(uploads);
+        if(uploads == []){     
+            res.render('projects.ejs',{msg:'No Projects To Show'});
+        }else{
+            res.render('projects.ejs',{uploads});
+        }
         
 
-//     }catch(err){
+    }catch(err){
         
-//         console.error(err.message);
-//         res.status(500).send('Server Error');
-//     }
-// });
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
-// // IOT
-// router.get('/ml', async function(req, res){
-//     try{
+// DSA
+router.get('/ml', async function(req, res){
+    try{
 
-//         const uploads = await Project.find({'domain': "IOT"}).populate('uploader',['rollNo', 'dept','year']);
-//         console.log(uploads);
-//         if(uploads == []){     
-//             res.render('projects.ejs',{msg:'No Projects To Show'});
-//         }else{
-//             res.render('projects.ejs',{uploads});
-//         }
+        const uploads = await Project.find({'domain': "DSA"}).populate('uploader',['rollNo', 'dept','year']);
+        console.log(uploads);
+        if(uploads == []){     
+            res.render('projects.ejs',{msg:'No Projects To Show'});
+        }else{
+            res.render('projects.ejs',{uploads});
+        }
         
 
-//     }catch(err){
+    }catch(err){
         
-//         console.error(err.message);
-//         res.status(500).send('Server Error');
-//     }
-// });
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// IOT
+router.get('/ml', async function(req, res){
+    try{
+
+        const uploads = await Project.find({'domain': "IOT"}).populate('uploader',['rollNo', 'dept','year']);
+        console.log(uploads);
+        if(uploads == []){     
+            res.render('projects.ejs',{msg:'No Projects To Show'});
+        }else{
+            res.render('projects.ejs',{uploads});
+        }
+        
+
+    }catch(err){
+        
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 router.get('/:id', async function(req, res){
     try{
 
+        console.log(user);
         const project = await Project.findOne({_id: req.params.id}).populate('uploader',['rollNo','dept','year']);
-        res.render('project.ejs',{project, logged:user.logged});
+        console.log(project);
+        res.render('project.ejs',{project});
 
     }catch(err){
         
